a85ef607b70ae650e0f6a9ba461ad74c4a6c0997
tgf <tuckergfisher@gmail.com>
2024-07-02 14:35:03 +0300

    clean up modules

--- alacritty/set_blur.py
+++ alacritty/set_blur.py
@@ -2,17 +2,22 @@ from pathlib import Path
 
 path = Path(__file__).parent
 
-with open(path.joinpath("alacritty.toml"), "r") as f:
-    lines = f.readlines()
+def toggle_blur():
 
-with open(path.joinpath(".old.alacritty.toml"), "w") as f:
-    f.writelines(lines)
+    with open(path.joinpath("alacritty.toml"), "r") as f:
+        lines = f.readlines()
 
-with open(path.joinpath("alacritty.toml"), "w") as f:
-    for line in lines:
-        if line.rstrip() == "blur = true":
-            f.write("blur = false\n")
-        elif line.rstrip().startswith("blur"):
-            f.write("blur = true\n")
-        else:
-            f.write(line)
+    with open(path.joinpath(".old.alacritty.toml"), "w") as f:
+        f.writelines(lines)
+
+    with open(path.joinpath("alacritty.toml"), "w") as f:
+        for line in lines:
+            if line.rstrip() == "blur = true":
+                f.write("blur = false\n")
+            elif line.rstrip().startswith("blur"):
+                f.write("blur = true\n")
+            else:
+                f.write(line)
+
+if __name__ == "__main__":
+    toggle_blur()
--- alacritty/set_opacity.py
+++ alacritty/set_opacity.py
@@ -2,17 +2,22 @@ from pathlib import Path
 
 path = Path(__file__).parent
 
-with open(path.joinpath("alacritty.toml"), "r") as f:
-    lines = f.readlines()
+def toggle_opacity():
 
-with open(path.joinpath(".old.alacritty.toml"), "w") as f:
-    f.writelines(lines)
+    with open(path.joinpath("alacritty.toml"), "r") as f:
+        lines = f.readlines()
 
-with open(path.joinpath("alacritty.toml"), "w") as f:
-    for line in lines:
-        if line.rstrip() == "opacity = 1.0":
-            f.write("opacity = 0.9\n")
-        elif line.rstrip().startswith("opacity"):
-            f.write("opacity = 1.0\n")
-        else:
-            f.write(line)
+    with open(path.joinpath(".old.alacritty.toml"), "w") as f:
+        f.writelines(lines)
+
+    with open(path.joinpath("alacritty.toml"), "w") as f:
+        for line in lines:
+            if line.rstrip() == "opacity = 1.0":
+                f.write("opacity = 0.9\n")
+            elif line.rstrip().startswith("opacity"):
+                f.write("opacity = 1.0\n")
+            else:
+                f.write(line)
+
+if __name__ == "__main__":
+    toggle_opacity()
